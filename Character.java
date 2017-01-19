7b192a500e7e521060a14ff93400b8e408f56cab
JetFire3201 <cymmm1@gmail.com>
2017-01-19 12:50:35 -0500

    Added Comments

--- Character.java
+++ Character.java
@@ -12,29 +12,49 @@ import java.util.ArrayList;
  * @author cymmm1
  */
 public class Character {
-         
+
     private String name;
     private int strength;
     private int health;
     private int speed;
     // fightType can be Knight, Magician, Fencer, Bandit.
     private String fightType;
+    private int defense;
+
     /**
+     *Makes a new Character Object
+     * 
+     * But this will not be used in conjunction to an assignment, simply as a argument in party class
      * 
      * @param name name of character
      * @param strength strength of character
      * @param health health of character
-     * @param speed  speed of character
-     * @param fighType
+     * @param speed speed of character
+     * @param fighType Class of character
      * @param fightType Character Class
      */
-    public Character(String name, int strength, int health,int speed, String fightclass){
+
+    public Character(String name, int strength, int health, int speed, String fightclass, int defense) {
         this.name = name;
         this.strength = strength;
         this.health = health;
         this.speed = speed;
         this.fightType = fightclass;
-        
+        this.defense = defense;
+
+    }
+
+    /**
+     *This is a class used for a dummy class.
+     */
+    public Character() {
+        this.name = "";
+        this.strength = 0;
+        this.health = 0;
+        this.speed = 0;
+        this.defense = 0;
+        this.fightType = "";
+
     }
 
     public String getFightType() {
@@ -44,7 +64,7 @@ public class Character {
     private void setFightType(String fightType) {
         this.fightType = fightType;
     }
-    
+
     public String getName() {
         return name;
     }
@@ -76,5 +96,5 @@ public class Character {
     private void setSpeed(int speed) {
         this.speed = speed;
     }
-    
+
 }
