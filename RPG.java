e9fe3ccbd9803c6cc1d9604a06017a1c2d7f494c
JetFire3201 <cymmm1@gmail.com>
2017-01-19 10:57:41 -0500

    Added the pointers to the Party Class and how to call them

--- RPG.java
+++ RPG.java
@@ -5,21 +5,33 @@
  */
 package rpg;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author cymmm1
  */
 public class RPG {
 
-    /**
-     * @param args the command line arguments
-     */
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        Character player = new Character("Matthew");
-        
-        System.out.println("Hello, " +player.getName() +".");
-        
+        boolean running = false;
+
+        Party party = new Party();
+
+        party.AddToParty(new Character("Matthew", 10, 100, 5, "Soldier"));
+        party.DisplayParty();
+
+        Character temp = new Character("John", 10, 100, 5, "Soldier");
+        party.AddToParty(temp);
+        party.DisplayParty();
+// below is just a test function to get names of the members in the party
+        System.out.println("got the name: " + (party.getCharacter("Matthe").getName()));
+       
     }
+
     
+
 }
