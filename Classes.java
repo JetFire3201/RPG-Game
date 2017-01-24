816c2fbc06562911a70dbb0496147a83a7cb6c6c
JetFire3201 <cymmm1@gmail.com>
2017-01-24 12:58:39 -0500

    Update Classes.Java
    I added an override method called toString that returns its name, not the enum class.

--- Classes.java
+++ Classes.java
@@ -10,5 +10,25 @@ package rpg;
  * @author cymmm1
  */
 public enum Classes {
-    WIZARD, KNIGHT;
-}
+    WIZARD("Wizard"), KNIGHT("Knight");
+    
+    public String value;
+    public int strength;
+    
+    private Classes(String value){
+        this.value = value;
+        strength = 100;
+    }
+    @Override
+    public String toString() { 
+        return this.value;
+        /* this is just a backup just in case
+        switch (this) { 
+            case WIZARD:
+                return "Wizard";
+            case KNIGHT:
+                return "Knight"; 
+        }
+         return super.toString();
+*/
+    }
